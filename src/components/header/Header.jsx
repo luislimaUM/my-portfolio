dff3ce2b4723afd792c3bc6b5d86f3c9839148eb
BOLD-2021051546\LuisMiguelLima <luis.soccer.5@hotmail.com>
2022-05-20 01:55:11 +0100

    add new components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
+import ME from '../../assets/me.jpg'
 
 const header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Luís Lima</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA/>
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href='#contact' className="scroll__down">Scroll Down</a>
+        <HeaderSocials/>
+      </div>
+    </header>
   )
 }
 
