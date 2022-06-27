ceae4b2ebd4212a1ca64b1d2c4e669ac72d07aa6
BOLD-2021051546\LuisMiguelLima <luis.soccer.5@hotmail.com>
2022-06-27 22:28:39 +0100

    add portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/bg-image.jpg'
 
 const Portfolio = () => {
   return (
-    <section id="portefolio">Portfolio</section>
+    <section id="portefolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt=""></img>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target="_blank">Github</a>
+          <a href="https://github.com" className='btn btn-primary' target="_blank">Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt=""></img>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target="_blank">Github</a>
+          <a href="https://github.com" className='btn btn-primary' target="_blank">Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
