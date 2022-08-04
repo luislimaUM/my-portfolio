45fcfa4fd19fd7c7d3d972cc9f81c4fd03a90b2d
BOLD-2021051546\LuisMiguelLima <luis.soccer.5@hotmail.com>
2022-08-04 23:41:58 +0100

    testimonials page

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './testimonials.css'
+import ME from '../../assets/me.png'
 
 const Testimonials = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className='testimonial'>
+          <div className='client__avatar'>
+            <img src={ME} alt="Avatar One"></img>
+            <h5>Luis Lima</h5>
+            <small className='client__review'>
+              Teste 123456 09876 kilojiugyufhkjoipoihjhfia
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
