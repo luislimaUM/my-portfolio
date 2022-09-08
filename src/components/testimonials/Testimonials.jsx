6563a9e9ede5f4987feb68fac9a18ec166053ed8
BOLD-2021051546\LuisMiguelLima <luis.soccer.5@hotmail.com>
2022-09-08 01:12:36 +0100

    swiper testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import './testimonials.css'
 import ME from '../../assets/me.png'
 
+// import Swiper core and required modules
+import {Pagination} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const data = [
   {
     avatar: ME,
@@ -12,6 +20,11 @@ const data = [
     avatar: ME,
     name: 'Luis Cunha',
     review: 'Teste'
+  },
+  {
+    avatar: ME,
+    name: 'Luis Cunha',
+    review: 'Teste111'
   }
 ]
 
@@ -21,11 +34,15 @@ const Testimonials = () => {
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{clickable: true}}>
       {
           data.map(({avatar, name, review}, index) => {
             return (
-              <article key={index} className='testimonial'>
+              <SwiperSlide key={index} className='testimonial'>
                 <div className='client__avatar'>
                   <img src={avatar}></img>
                 </div>
@@ -33,11 +50,11 @@ const Testimonials = () => {
                 <small className='client__review'>
                   {review}
                 </small>
-              </article>
+              </SwiperSlide>
             )
           })
         }
-      </div>
+      </Swiper>
     </section>
   )
 }
